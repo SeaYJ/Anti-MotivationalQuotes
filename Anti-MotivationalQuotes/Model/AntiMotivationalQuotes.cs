b9d4123b9c504e735c08e7f5fe0ce347cd203caa
SeaYJ <seayj21@outlook.com>
2023-07-20 18:28:58 +0800

    进步空间还很大...

---
+++ Anti-MotivationalQuotes/Model/AntiMotivationalQuotes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anti_MotivationalQuotes.Model
+{
+    internal class AntiMotivationalQuotes
+    {
+        private int _MAX_STEP = 10;
+        public int MAX_STEP { get { return _MAX_STEP; } }
+
+        private string[] quotesList;
+        public string[] QuotesList { get { return quotesList; } set { quotesList = value; } }
+
+        private int showIndex = 0;
+        public int ShowIndex { get { return showIndex; } set { showIndex = value; } }
+
+        private int step = 1;
+        public int Step { get { return step; } set { step = value; } }
+
+        private int lines = 0;
+        public int Lines { get { return lines; } set { lines = value; } }
+    }
+}
---
+++ Anti-MotivationalQuotes/View/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Anti_MotivationalQuotes.Model;
+
+namespace Anti_MotivationalQuotes.View
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        AntiMotivationalQuotes Quotes;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Quotes = new AntiMotivationalQuotes();
+            ReadAllAnti_MotivationalQuotes();
+
+            Quotes.ShowIndex = GetRandomIntValue(0, Quotes.Lines);
+
+            this.Anti_MotivationalQuotesShowBlock.Text = Quotes.QuotesList[Quotes.ShowIndex];
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void WindowCloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void WindowMinimizeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void WindowStateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            switch (this.WindowState)
+            {
+                case WindowState.Maximized:
+                    this.WindowState = WindowState.Normal;
+                    this.WindowStateBtn.Content = "\xeda1";
+                    break;
+
+                case WindowState.Normal:
+                    this.WindowState = WindowState.Maximized;
+                    this.WindowStateBtn.Content = "\xeda0";
+                    break;
+
+                default:
+                    this.WindowState = WindowState.Normal;
+                    this.WindowStateBtn.Content = "\xeda1";
+                    break;
+            }
+        }
+
+        private void AnotherBillionBowlsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Quotes.Step = GetRandomIntValue(0, Quotes.MAX_STEP);
+            Quotes.ShowIndex = (Quotes.ShowIndex + Quotes.Step) % Quotes.Lines;
+            this.Anti_MotivationalQuotesShowBlock.Text = Quotes.QuotesList[Quotes.ShowIndex];
+        }
+
+        private void ReadAllAnti_MotivationalQuotes()
+        {
+
+            try
+            {
+                string DataPath = "Anti-MotivationalQuotes";
+
+                StreamReader sr = new StreamReader(DataPath);
+
+                string t = sr.ReadToEnd();
+
+                Quotes.QuotesList = t.Split('\n');
+                Quotes.Lines = Quotes.QuotesList.Length;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+                this.Close();
+            }
+        }
+
+        private int GetRandomIntValue(int minV, int maxV)
+        {
+            return (new Random()).Next(minV, maxV); ;
+        }
+    }
+}
